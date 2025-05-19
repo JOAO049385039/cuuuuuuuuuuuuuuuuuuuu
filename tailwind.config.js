1a0bcf40ea60ea975d626cae86e635a39ff35f0c
JOAO049385039 <geral@divinoencanta.com>
2025-05-19 02:53:14 +0100

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,13 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       colors: {
-        marsala: '#6c2e33',
-        dourado: '#b76e79',
-        rosaclaro: '#f8e1e7'
-      }
-    }
+        rosaClaro: '#f8e1e7',
+        douradoRose: '#b76e79',
+        marsala: '#6e1e2a',
+      },
+    },
   },
   plugins: [],
 }
