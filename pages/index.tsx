feecf99d1200786658ee3d8aaaf6da3b41f48ac5
JOAO049385039 <geral@divinoencanta.com>
2025-05-19 01:01:20 +0100

    Add files via upload

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,7 @@
-import Link from "next/link";
-
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-pink-50 text-center">
-      <h1 className="text-3xl font-bold text-rose-600">Bem-vindo à Divino Encanto</h1>
-      <p className="text-lg mt-2 text-gray-700">Explore nossos produtos encantadores!</p>
-      <Link href="/produtos" className="mt-4 text-pink-600 hover:underline">
-        Ver Produtos
-      </Link>
+    <div className="min-h-screen flex items-center justify-center">
+      <h1 className="text-3xl font-bold text-indigo-600">Bem-vindo ao Divino Encanto</h1>
     </div>
-  );
+  )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,7 @@
 module.exports = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-    "./context/**/*.{js,ts,jsx,tsx}"
-  ],
+  content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {
-      colors: {
-        rose: {
-          600: '#912F40',
-        },
-        pink: {
-          50: '#fbeaed',
-          600: '#D46A92',
-        }
-      }
-    }
+    extend: {},
   },
   plugins: [],
 }
